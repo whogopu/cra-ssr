373b3c6b1c3a76ef9238b190e632e7a02a178a6a
Gopal sharma <gopal151295@gmail.com>
2019-07-05 02:55:31 +0530

    cra+ssr

---
+++ server/middleware/renderer.js
@@ -0,0 +1,54 @@
+import React from 'react'
+import ReactDOMServer from 'react-dom/server'
+
+// import our main App component
+import App from '../../src/App';
+
+const path = require("path");
+const fs = require("fs");
+import Loadable from 'react-loadable';
+
+import manifest from '../../build/asset-manifest.json';
+
+// function to extract js assets from the manifest
+const extractAssets = (assets, chunks) => Object.keys(assets)
+    .filter(asset => chunks.indexOf(asset.replace('.js', '')) > -1)
+    .map(k => assets[k]);
+
+export default (req, res, next) => {
+
+    // point to the html file created by CRA's build tool
+    const filePath = path.resolve(__dirname, '..', '..', 'build', 'index.html');
+
+    fs.readFile(filePath, 'utf8', (err, htmlData) => {
+        if (err) {
+            console.error('err', err);
+            return res.status(404).end()
+        }
+
+        const modules = [];
+
+        // render the app as a string
+        const html = ReactDOMServer.renderToString(
+            <Loadable.Capture report={m => modules.push(m)}>
+                <App/>
+            </Loadable.Capture>
+        );
+
+        const extraChunks = extractAssets(manifest['files'], modules)
+                            .map(c => `<script type="text/javascript" src="${c}"></script>`);
+
+        // inject the rendered app into our html and send it
+        return res.send(
+            htmlData
+            .replace(
+                '<div id="root"></div>',
+                `<div id="root">${html}</div>`
+            )
+            .replace(
+                '</body>',
+                extraChunks.join('') + '</body>'
+            )
+        );
+    });
+}
--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
 import React from 'react';
+import Loadable from 'react-loadable';
 import logo from './logo.svg';
 import './App.css';
+
+const AsyncComponent = Loadable({
+  loader: () => import(/* webpackChunkName: "myNamedChunk" */ "./SomeComponent"),
+  loading: () => <div>loading...</div>,
+  modules: ['myNamedChunk']
+});
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>App compoent</div>
+      <AsyncComponent/>
     </div>
   );
 }
