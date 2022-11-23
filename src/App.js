072042ba210fc2a0cd9915fc1f172329d9026d7a
Gopal sharma <gopal.sharma@99acres.com>
2022-11-23 15:17:21 +0530

    react ssr working

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
+/** @jsx jsx */ 
 import React from 'react';
 import Loadable from 'react-loadable';
 import logo from './logo.svg';
 import './App.css';
+import { css, jsx } from "@emotion/react";
+import { baseIcon_style, bgSizeHeightWidth_COMMON_w24_starHover_style, bgSizeHeightWidth_COMMON_w24_style, bg_url_COMMON_style } from './styles';
 
 const AsyncComponent = Loadable({
   loader: () => import(/* webpackChunkName: "myNamedChunk" */ "./SomeComponent"),
@@ -11,9 +14,22 @@ const AsyncComponent = Loadable({
 
 function App() {
   return (
-    <div className="App">
-      <div>App compoent</div>
-      <AsyncComponent/>
+    <div className="App" css={{ color: 'hotpink' }} >
+      <div><i
+        css={[baseIcon_style,
+          bg_url_COMMON_style,
+          bgSizeHeightWidth_COMMON_w24_style,
+          bgSizeHeightWidth_COMMON_w24_starHover_style
+        ]}
+      ></i></div>
+      <div><i
+        css={[baseIcon_style,
+          bg_url_COMMON_style,
+          bgSizeHeightWidth_COMMON_w24_style,
+          bgSizeHeightWidth_COMMON_w24_starHover_style
+        ]}
+      ></i></div>
+      <AsyncComponent />
     </div>
   );
 }
---
+++ src/styles.js
@@ -0,0 +1,29 @@
+import { css } from "@emotion/react";
+// import { css } from "@emotion/css";
+
+// base icon styles
+export const baseIcon_style = css`
+    display : inline-block;
+    vertical-align : middle;
+    font-style : normal;
+    background-repeat : no-repeat;
+`
+
+// sprite url style
+export const bg_url_COMMON_style = css`
+    background: url(https://static.99acres.com/universalapp/img/CommonSprite_v40.png);
+`
+export const bg_url_SRP_MOBILE_style = css`
+    background: url(https://static.99acres.com//universalapp/img/srpmobile_v8.png);
+`
+
+
+export const bgSizeHeightWidth_COMMON_w24_style = css`
+    width: 24px;
+    height: 24px;
+    background-size: 408px 288px;
+`
+
+export const bgSizeHeightWidth_COMMON_w24_starHover_style = css`
+    background-position: -96px -0px;
+`
